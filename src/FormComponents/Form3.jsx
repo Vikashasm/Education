d85890e89906bbb02379284dcfcb09af07611d8e
Dhruv-swami <swamidhruv058@gmail.com>
2024-04-21 17:41:14 +0530

    ui update

--- src/FormComponents/Form3.jsx
+++ src/FormComponents/Form3.jsx
@@ -36,55 +36,49 @@ function Form3({ onSubmit }) {
     onSubmit(formData); // Submit the form data if validation passes
   };
   return (
-    <div className=" bg_img h-screen w-[100%] md:w-[79.3%]  md:py-4 relative overflow-y-scroll flex items-center justify-center">
+    <div className="h-screen w-[100%] md:w-[79.3%]  md:py-4 relative flex items-center justify-center m-auto">
       <div className="w-full h-[600px] sm:h-auto pb-4">
-        <div className=" bg-white rounded-[20px] p-10 lg:p-6 xl:p-10 w-[90%] md:w-[460px] mx-auto">
+        <div className=" bg-white rounded-[20px] p-10 lg:p-6 xl:p-10 w-[90%] md:w-[800px] mx-auto">
           <h2 className=" text-[#FF2000] text-center text-xl font-medium mb-8 lg:mb-6 xl:mb-8">
-            Before you dive into your results, hang tight for just two more
-            questions!
+            Before you dive into your results, hang tight for just two more questions!
           </h2>
           <form>
             <div className=" flex flex-col gap-8 lg:gap-5 xl:gap-8">
-              <div className=" flex flex-col gap-3">
-                <label
-                  className=" text-[#125566] text-sm font-normal"
-                  htmlFor="School"
-                >
-                  Which school or college did you last grace with your presence?
-                </label>
-                <input
-                  className=" bg-[#EEEEEE] rounded-xl py-[12px] px-[20px] text-[#00000080] font-normal text-base outline-none"
-                  type="text"
-                  id="School"
-                  name="School"
-                  placeholder="Enter Your Answer"
-                  value={formData.School}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className=" flex flex-col gap-3">
-                <label
-                  className=" text-[#125566] text-sm font-normal"
-                  htmlFor="Age"
-                >
-                  Also, spill the beans – how many trips around the sun have you
-                  completed?" I mean your Age.
-                </label>
-                <input
-                  className=" bg-[#EEEEEE] rounded-xl py-[12px] px-[20px] text-[#00000080] font-normal text-base outline-none"
-                  type="text"
-                  id="Age"
-                  name="Age"
-                  placeholder="Enter Your Answer"
-                  value={formData.Age}
-                  onChange={handleChange}
-                />
+              <div className="flex flex-col md:flex-row">
+                <div className=" flex flex-col gap-3 w-full">
+                  <label className=" text-[#125566] text-sm font-normal" htmlFor="School">
+                    Which school or college did you last grace with your presence?
+                  </label>
+                  <input
+                    className=" bg-[#EEEEEE] rounded-xl py-[12px] px-[20px] text-[#00000080] font-normal text-base outline-none w-full"
+                    type="text"
+                    id="School"
+                    name="School"
+                    placeholder="Enter Your Answer"
+                    value={formData.School}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className=" flex flex-col gap-3 w-full">
+                  <label className=" text-[#125566] text-sm font-normal" htmlFor="Age">
+                    Also, spill the beans – how many trips around the sun have you completed?" I
+                    mean your Age.
+                  </label>
+                  <input
+                    className=" bg-[#EEEEEE] rounded-xl py-[12px] px-[20px] text-[#00000080] font-normal text-base outline-none w-full"
+                    type="text"
+                    id="Age"
+                    name="Age"
+                    placeholder="Enter Your Answer"
+                    value={formData.Age}
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
               <div className=" text-end">
                 <button
                   onClick={handleSubmit}
-                  className=" text-white text-lg font-normal md:bg-[#66BCB4] bg-[#FF2000] rounded-[10px] py-[10px] w-full md:w-auto px-[30px] text-center"
-                >
+                  className=" text-white text-lg font-normal md:bg-[#66BCB4] bg-[#FF2000] rounded-[10px] py-[10px] w-full md:w-auto px-[30px] text-center">
                   Submit for Final Result
                 </button>
               </div>
